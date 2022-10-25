43f9ef34a3dedff8b376eee42729cf91b5043138
Sean McMillan <reaper@umich.edu>
2022-10-25 13:52:24 -0400

    Updates

--- bin/mklinks.bat
+++ bin/mklinks.bat
@@ -3,4 +3,5 @@ mklink %USERPROFILE%\.gitconfig %CD%\.gitconfig
 mklink %USERPROFILE%\.gitignore_global %CD%\.gitignore_global
 mklink %USERPROFILE%\.profile %CD%\.profile
 mklink /D %USERPROFILE%\.vim %CD%\.vim
+mklink /D %USERPROFILE%\vimfiles %CD%\.vim
 mklink %USERPROFILE%\.vimrc %CD%\.vimrc
